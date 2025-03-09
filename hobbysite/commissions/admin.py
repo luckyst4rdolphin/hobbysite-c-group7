9541a7d334bce2085ecbef0b56bec290c82f6106
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-09 20:58:39 +0800

    Added admin panels for each commissions app models

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Commission, Comment
 
 # Register your models here.
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Comment, CommentAdmin)
