fb770d6f2cbc97da6d3d5924579117900660b1ca
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-09 20:33:33 +0800

    Created models for commissions app

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    Commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
