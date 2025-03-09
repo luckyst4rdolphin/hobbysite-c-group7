23c11c1b4f662134e4daf5d8b05b6bd57fda9df7
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-10 07:20:14 +0800

    created list view for article categories

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from blog.models import ArticleCategory, Article
 
-# Create your views here.
+class ArticleCategoryListView(ListView):
+    context_object_name = "categories"
+    queryset = ArticleCategory.objects.all().order_by('name')
+    template_name = 'article_category_list.html'
+
+
+
