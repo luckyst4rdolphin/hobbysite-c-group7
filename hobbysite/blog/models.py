b761b364962fe68905f4e651499c6cbbcb459679
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-09 17:45:16 +0800

    Added two models

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
