ade105b54b6440178a998752ab87ad898dc22354
macasalad <hannahleibaniqued12@gmail.com>
2025-03-10 17:24:52 +0800

    Created products and product views

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def products(request):
+    products = Product.objects.all()
+    ctx = {
+        "products": products
+    }
+
+    return render(request, "merch_list.html", ctx)
+
+def product(request, pk):
+    product = Product.objects.get(pk = pk)
+    ctx = {
+        "product": product,
+    }
+
+    return render(request, "merch_detail.html", ctx)
