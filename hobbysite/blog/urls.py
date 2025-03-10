757d20615d3c485d019bd12e7376eeb4c052ad09
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-10 11:14:42 +0800

    Added url pattern for article detailed view

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import ArticleCategoryListView
+from blog.views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('blog/articles', ArticleCategoryListView.as_view(), name="acrticle-category-list"),
+   path('blog/articles', ArticleListView.as_view(), name="acrticle-list"),
+   path('blog/article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
 ]
 
 app_name = "blog"
