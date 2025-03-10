ef26f3a2c00fd867d6d24866367c05258cf7dca6
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-10 11:13:59 +0800

    Added meta inner class

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
@@ -19,5 +23,11 @@ class Article(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:article-detail', args=[self.pk])
