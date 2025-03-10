f5dc5edb6ab5e13a6605ad49bbf6c873bf6335cb
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-10 11:22:02 +0800

    Modified models for commissions app. Added meta classes for data sorting

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -2,18 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Commission(models.Model):
+
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
     description = models.TextField()
     people_required = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
 class Comment(models.Model):
-    Commission = models.ForeignKey(
+    commission = models.ForeignKey(
         Commission,
-        on_delete=models.CASCADE,
-        related_name="comments"
+        on_delete=models.CASCADE
     )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
