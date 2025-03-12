7a802ad8d2ba92ae4377739a7dd3f2eba1347714
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-13 01:56:58 +0800

    Constructed built-in generic views for forum app

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from forum.models import PostCategory, Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    context_object_name = 'posts'
+    queryset = Post.objects.all()
+    template_name = 'post_list.html'
+
+    # To access & display each categories within PostCategory as well...
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = PostCategory.objects.all()
+        return context
+
+
+class PostDetailView(DetailView):
+    context_object_name = "post"
+    model = Post
+    template_name = 'post.html'
