edf0d1b2f1ff97f0da62b33e363bc405919bc4d8
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-13 02:50:34 +0800

    Defined the URL pattern for respective PostDetailView instances within the forum app

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -3,4 +3,5 @@ from forum.views import PostListView, PostDetailView
 
 urlpatterns = [
     path('threads/', PostListView.as_view(), name='threads'),
-]
+    path('thread/<int:pk>/', PostDetailView.as_view(), name='thread'),
+    ]
