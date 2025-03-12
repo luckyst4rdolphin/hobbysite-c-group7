a790f707080b78cba88eb5bd41c0c922d90ac93a
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-12 17:56:31 +0800

    Added additional context for article list view

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from blog.models import Article
+from blog.models import Article, ArticleCategory
 
 class ArticleListView(ListView):
     context_object_name = "articles"
     queryset = Article.objects.all().order_by('-created_on')
     template_name = 'article_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ArticleCategory.objects.all().order_by('name')
+        return context
 
 class ArticleDetailView(DetailView):
     context_object_name = "article"
