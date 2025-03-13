b3ed42149fd63dd32518aeab3be58aa093675476
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-13 18:13:22 +0800

    Modified and fixed get_absolute_url function

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,13 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Commission(models.Model):
 
     title = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
-    
     description = models.TextField()
     people_required = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -15,11 +12,14 @@ class Commission(models.Model):
 
     class Meta:
         ordering = ["created_on"]
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     commission = models.ForeignKey(
         Commission,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -27,3 +27,9 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
+
+    def ___str___(self):
+        return self.commission
+    
+    def get_absolute_url(self):
+        return reverse('commissions:commissions-detail', args=[self.pk])
