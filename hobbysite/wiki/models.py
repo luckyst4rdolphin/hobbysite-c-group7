cf90dd34e526d0f1b97296cb18ecb7e32253a995
chrisxdale <chrisxdalelopez@gmail.com>
2025-03-14 06:02:51 +0800

    Fixed models.py
    Added a set null true and added Meta classes for each class model.

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -16,10 +16,19 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(
         ArticleCategory, 
-        on_delete=models.SET_NULL)
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        )
     entry = models.TextField
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_on']
+
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('wiki:article-detail', args=[self.pk])
