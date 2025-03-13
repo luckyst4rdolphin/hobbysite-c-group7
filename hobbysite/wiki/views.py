1f17099829359cdf488181ed305291968fae86c1
chrisxdale <chrisxdalelopez@gmail.com>
2025-03-14 05:55:33 +0800

    Created ListView and DetailView
    Added class-based views, ListView and DetailView, under views.py in the wiki app.

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from wiki.models import Article
 
 # Create your views here.
+class ArticleListView(ListView):
+    context_object_name = 'articles'
+    queryset = Article.objects.all().order_by('-created_on')
+    template_name = 'article_categories.html'
+
+class ArticleDetailView(ListView):
+    context_object_name = 'article'
+    model = Article
+    template_name = 'article.html'
+
