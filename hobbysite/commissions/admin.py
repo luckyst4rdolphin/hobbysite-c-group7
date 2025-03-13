14f5cfb4442c286a63c430a45767d0cc00512066
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-13 18:20:59 +0800

    Fixed admin panels for the models

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -4,9 +4,11 @@ from .models import Commission, Comment
 # Register your models here.
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
+    list_display = ('title', 'people_required', 'created_on', 'updated_on')
 
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
+    list_display = ('commission', 'entry', 'created_on', 'updated_on')
 
 admin.site.register(Commission, CommissionAdmin)
 admin.site.register(Comment, CommentAdmin)
