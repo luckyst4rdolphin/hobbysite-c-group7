b8a94fa8919a2a7333e2514307feec3198929820
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-14 20:43:09 +0800

    Added docstrings to views.py

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -3,18 +3,28 @@ from forum.models import PostCategory, Post
 
 
 class PostListView(ListView):
+    """
+    A view that displays all categories, together with their respective threads.
+    """
+
     context_object_name = 'posts'
     queryset = Post.objects.all()
     template_name = 'post_list.html'
 
-    # To access & display each categories within PostCategory as well...
+    
     def get_context_data(self, **kwargs):
+        """
+        Adds thread categories to the context.
+        """
         context = super().get_context_data(**kwargs)
         context['categories'] = PostCategory.objects.all()
         return context
 
 
 class PostDetailView(DetailView):
+    """
+    Displays details of a single thread.
+    """
     context_object_name = "post"
     model = Post
     template_name = 'post.html'
