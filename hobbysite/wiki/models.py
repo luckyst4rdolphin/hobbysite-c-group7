6a50108e60ee2668b4a2a7898343313f50020431
chrisxdale <chrisxdalelopez@gmail.com>
2025-03-14 17:03:23 +0800

    Added documentation for models.py
    Added class descriptions for each class and also added comments for functions.

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,19 +1,43 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 class ArticleCategory(models.Model):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+    
+    This class is the model class for Article Category
+    This contains the specified parameters for name and description
+    required in the specs of the project.
+
+    '''
     name = models.CharField(max_length=255)
     description = models.TextField()
 
     class Meta:
+        '''
+        @authors : Antonth Chrisdale C. Lopez
+    
+        This class is the Meta inner class of the article category model
+
+        '''
+        
         ordering = ['name']
         verbose_name_plural = 'Article Categories'
 
     def __str__(self):
         return self.name
-    
+
 class Article(models.Model):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+    
+    This class is the model class for Article
+    This contains the specified parameters for title, category,
+    entry, created_on, and updated_on required in the specs of 
+    the project.
+
+    '''
+
     title = models.CharField(max_length=255)
     category = models.ForeignKey(
         ArticleCategory, 
@@ -26,10 +50,27 @@ class Article(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
+        '''
+        @authors : Antonth Chrisdale C. Lopez
+    
+        This class is the model class for Article
+        This contains the specified parameters for title, category,
+        entry, created_on, and updated_on required in the specs of 
+        the project.
+
+        '''
         ordering = ['-created_on']
 
     def __str__(self):
+        '''
+        @fn __str__
+        @brief returns the title of the object
+        '''
         return self.title
     
     def get_absolute_url(self):
+        '''
+        @fn get_absolute_url
+        @brief returns url of the article
+        '''
         return reverse('wiki:article-detail', args=[self.pk])
