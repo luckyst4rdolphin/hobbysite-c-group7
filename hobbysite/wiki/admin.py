eb09126327e57c2064904bec3d24e09f44364b42
chrisxdale <chrisxdalelopez@gmail.com>
2025-03-14 16:46:54 +0800

    Added documentations for admin.py
    Added class-based documentations for the class found in the admin.py file: ArticleCategoryAdmin and ArticleAdmin.

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from .models import ArticleCategory, Article
 
-# Register your models here.
 class ArticleCategoryAdmin(admin.ModelAdmin):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+
+    This class is the admin class of the ArticleCategory model.
+
+    '''
     model = ArticleCategory
 
     list_display = ('name',)
 
 class ArticleAdmin(admin.ModelAdmin):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+    
+    This class is the admin class of the Article model.
+
+    '''
     model = Article
 
     list_display = ('title', 'category', 'entry', 'created_on', 'updated_on')
