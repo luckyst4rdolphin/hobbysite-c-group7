0518d3cae0900c361fc275730d79a7088255f3be
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-14 15:40:55 +0800

    Modified and finalized views for commissions app

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -3,10 +3,11 @@ from commissions.models import Commission, Comment
 
 class CommissionListView(ListView):
     context_object_name = "commissions"
-    queryset = Comment.objects.all().order_by('created_on')
+    queryset = Commission.objects.all().order_by('created_on')
     template_name = "commission_list.html"
 
 class CommissionDetailView(DetailView):
     context_object_name = "commission"
-    model = Comment
+    model = Commission
+    extra_context = {"comments":Comment.objects.all()}
     template_name = "commission.html"
