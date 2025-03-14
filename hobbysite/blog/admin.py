3101bc33fd86d0e44c9c2b1275124f0a3cd27220
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-14 21:24:35 +0800

    Added docstrings for admin.py

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import ArticleCategory, Article
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
+    '''
+    This is the admin panel for the Article Category model.
+    '''
     model = ArticleCategory
 
     list_display = ('name', )
 
 class ArticleAdmin(admin.ModelAdmin):
+    '''
+    This is the admin panel for the Article model.
+    '''
     model = Article
 
     list_display = ('title', 'category', 'entry', 'created_on', 'updated_on')
