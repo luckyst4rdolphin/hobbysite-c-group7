fa0706a90f4c1684ce029b219b49264ab8aadc85
jcdc-chewy <jcdc_chewy@proton.me>
2025-03-14 22:08:30 +0800

    Configured admin panels for each forum app models

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from forum.models import PostCategory, Post
 
-admin.site.register(Post)
-admin.site.register(PostCategory)
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Post model.
+    """
+    model = Post
+    list_display = ('title',)
+    search_fields = ('title',)
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for PostCategory model.
+    """
+    model = PostCategory
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
