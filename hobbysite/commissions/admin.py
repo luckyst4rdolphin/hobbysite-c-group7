df9c39d970a171c6149ef35e9554c7fe310e6d36
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-14 22:57:20 +0800

    Added docstring for admin.py and removed unnecessary CommentInLine function

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 from .models import Commission, Comment
 
 # Register your models here.
-
-class CommentInline(admin.TabularInline):
-    model = Comment
-
 class CommissionAdmin(admin.ModelAdmin):
+    '''
+    Admin configurationn for the commission model.
+    '''
     model = Commission
     list_display = ('title', 'people_required', 'created_on', 'updated_on')
 
 class CommentAdmin(admin.ModelAdmin):
+    '''
+    Admin configuration for the comment model.
+    '''
     model = Comment
     list_display = ('commission', 'entry', 'created_on', 'updated_on')
 
