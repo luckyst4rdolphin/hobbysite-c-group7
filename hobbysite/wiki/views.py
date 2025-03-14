82fcafd33db18fbfc2454046591e7d3b4b08b863
chrisxdale <chrisxdalelopez@gmail.com>
2025-03-14 09:36:50 +0800

    Updated views.py under wiki app
    Added the get_context_data function and the ArticleCategory import.

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from wiki.models import Article
+from wiki.models import Article, ArticleCategory
 
 # Create your views here.
 class ArticleListView(ListView):
     context_object_name = 'articles'
     queryset = Article.objects.all().order_by('-created_on')
     template_name = 'article_categories.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ArticleCategory.objects.all().order_by('name')
+        return context
 
 class ArticleDetailView(ListView):
     context_object_name = 'article'
