e0c07db5b96d2fdc85b85d1caa70b3942ece9fcb
macasalad <hannahleibaniqued12@gmail.com>
2025-05-08 15:41:16 +0800

    Added home in urlpatterns

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomePageView
 
 urlpatterns = [
+    path('home/', HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('commissions', include('commissions.urls')),
