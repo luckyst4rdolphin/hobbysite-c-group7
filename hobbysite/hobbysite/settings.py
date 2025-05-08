68627701683636507c3b2993cdf4c67f2a4c5407
macasalad <hannahleibaniqued12@gmail.com>
2025-05-08 15:39:44 +0800

    Added login redirect url

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
