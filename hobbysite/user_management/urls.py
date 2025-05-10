90214543158d6cad0d00c87b0ae48f9ecff9ccf2
macasalad <hannahleibaniqued12@gmail.com>
2025-05-10 13:42:30 +0800

    Fixed urls for user_management

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -3,7 +3,7 @@ from user_management.views import UserCreateView, ProfileEdit
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='register_user'),
-    path('profile/', ProfileEdit.as_view(), name='profile_edit'),
+    path('profile/', ProfileEdit.as_view(), name='update_profile'),
     ]
 
 app_name = 'user_management'
