54f233f323a0b451909ddd5487762c17ea0fb8d4
chrisxdale <chrisxdalelopez@gmail.com>
2025-05-12 05:23:52 +0800

    Created ArticleCreateForm and ArticleUpdateForm in forms.py
    Added ArticleCreateForm and ArticleUpdateForm in forms.py which both contains the respective fields specified in the specs.

---
+++ hobbysite/wiki/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Article
+ 
+class ArticleCreateForm(forms.ModelForm):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+    
+    This class contains the form for creating Articles
+
+    '''
+    class Meta:
+        '''
+        @authors : Antonth Chrisdale C. Lopez
+    
+        This class contains the form for creating Articles
+
+        '''
+        model = Article
+        fields = "__all__"
+
+class ArticleUpdateForm(forms.ModelForm):
+    '''
+    @authors : Antonth Chrisdale C. Lopez
+    
+    This class contains the form for updating existing
+    Articles.
+
+    '''
+    class Meta:
+        '''
+        @authors : Antonth Chrisdale C. Lopez
+    
+        This class contains the form for updating existing
+        Articles.
+
+        '''
+        model = Article
+        fields = ['title', 'catgeory', 'entry', 'header_image', 'updated_on']
