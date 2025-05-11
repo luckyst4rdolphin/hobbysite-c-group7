58d18c8d6a16f006be7c1f8c9b0a655570225041
shamika-anne <shamanne.sawalha@gmail.com>
2025-05-11 23:25:25 +0800

    added url for article form

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import ArticleListView, ArticleDetailView
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView
 
 urlpatterns = [
-   path('articles/', ArticleListView.as_view(), name="acrticle-list"),
+   path('articles/', ArticleListView.as_view(), name="article-list"),
    path('article/<int:pk>/', ArticleDetailView.as_view(), name="article-detail"),
+   path('article/add/', ArticleCreateView.as_view(), name="article-form"),
 ]
 
 app_name = "blog"
