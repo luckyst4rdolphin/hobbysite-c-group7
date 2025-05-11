bb2a0dd9d7865cc81d719e0f27075b62ad50c15d
macasalad <hannahleibaniqued12@gmail.com>
2025-05-11 21:51:17 +0800

    Added url for adding product

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MerchListView, MerchDetailView
+from .views import MerchListView, MerchDetailView, MerchCreateView
 
 urlpatterns = [
     path('items/', MerchListView.as_view(), name="merch_list"),
-    path('item/<int:pk>/', MerchDetailView.as_view(), name='merch_detail')
+    path('item/<int:pk>/', MerchDetailView.as_view(), name='merch_detail'),
+    path('item/add/', MerchCreateView.as_view(), name='merch_create')
 ]
 
 app_name = "merchstore"
