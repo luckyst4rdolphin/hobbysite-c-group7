ad60b0a49827713614bd484a5ff4aecf88e1242c
jcdc-chewy <jcdc_chewy@proton.me>
2025-05-12 06:02:21 +0800

    Defined the URL pattern for ThreadCreateView

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -4,6 +4,7 @@ from forum.views import ThreadListView, ThreadDetailView
 urlpatterns = [
     path('threads/', ThreadListView.as_view(), name='thread-list'),
     path('thread/<int:pk>/', ThreadDetailView.as_view(), name='thread-detail'),
+    path('thread/add/', ThreadCreateView.as_view(), name='thread-create'),
     ]
 
 app_name = 'forum'
