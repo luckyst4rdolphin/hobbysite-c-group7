fcc50dac3b80f47baeae98e7d399c4c3333f01a1
jcdc-chewy <jcdc_chewy@proton.me>
2025-05-12 01:13:19 +0800

    Fixed urls

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from forum.views import ThreadListView, ThreadDetailView
 
 urlpatterns = [
-    path('threads/', PostListView.as_view(), name='posts_list'),
-    path('thread/<int:pk>/', PostDetailView.as_view(), name='post_showcase'),
+    path('/threads/', ThreadListView.as_view(), name='posts_list'),
+    path('/thread/<int:pk>/', ThreadDetailView.as_view(), name='post_showcase'),
     ]
 
 app_name = 'forum'
