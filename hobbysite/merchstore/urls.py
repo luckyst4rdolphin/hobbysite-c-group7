d1d151ddf43c113d344a6256900d02a3dd58de5a
macasalad <hannahleibaniqued12@gmail.com>
2025-05-11 22:21:46 +0800

    Added url for editing own products

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MerchListView, MerchDetailView, MerchCreateView
+from .views import MerchListView, MerchDetailView, MerchCreateView, MerchUpdateView
 
 urlpatterns = [
     path('items/', MerchListView.as_view(), name="merch_list"),
     path('item/<int:pk>/', MerchDetailView.as_view(), name='merch_detail'),
+    path('item/<int:pk>/edit', MerchUpdateView.as_view(), name='merch_update'),
     path('item/add/', MerchCreateView.as_view(), name='merch_create')
 ]
 
