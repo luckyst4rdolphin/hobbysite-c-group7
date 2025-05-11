81d27933f5e44792d5b50708575ab731c2489d77
macasalad <hannahleibaniqued12@gmail.com>
2025-05-11 21:43:56 +0800

    Added TransactionAdmin

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
 
 
 class ProductTypeAdmin(admin.ModelAdmin):
@@ -25,6 +25,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('product_type',)
     search_fields = ('name', 'product_type__name')
 
+class TransactionAdmin(admin.ModelAdmin):
+    '''
+    Admin configuration for Product model
+    '''
+    list_display = ('buyer', 'product', 'amount', 'status')
+    list_filter = ('product',)
+    search_fields = ('buyer', 'product__name')
+
 # Register models with their corresponding admin configurations
 admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
