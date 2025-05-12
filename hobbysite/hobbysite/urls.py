5ff7f5228f2af5a34e7f02932667905b3d51d3e1
chrisxdale <chrisxdalelopez@gmail.com>
2025-05-12 21:05:56 +0800

    Removed settings debug in url.py

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -15,7 +15,3 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('wiki/', include('wiki.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root = settings.MEDIA_ROOT)
