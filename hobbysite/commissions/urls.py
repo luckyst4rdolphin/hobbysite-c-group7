401382a58ba43301cdec99c6ed0a6d69863ad3f7
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-05-12 09:57:31 +0800

    updated commission urls

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CommissionListView, CommissionDetailView
+from .views import CommissionListView, CommissionDetailView, CommissionCreateView, CommissionUpdateView
 
 urlpatterns = [
     path('list/', CommissionListView.as_view(), name="commissions-list"),
     path('detail/<int:pk>', CommissionDetailView.as_view(), name="commissions-detail"),
+    path('add/', CommissionCreateView.as_view(), name="commissions-create"),
+    path('<int:pk>/edit/', CommissionUpdateView.as_view(), name="commissions-update"),
 ]
 
 app_name = "commissions"
