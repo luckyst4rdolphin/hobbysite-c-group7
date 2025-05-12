dcf1e6bde9dd0afde8ae3002f6b9c78a7e385088
macasalad <hannahleibaniqued12@gmail.com>
2025-05-12 11:20:36 +0800

    Removed LoginRequired in HomePageView

--- hobbysite/hobbysite/views.py
+++ hobbysite/hobbysite/views.py
@@ -1,6 +1,4 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePageView(LoginRequiredMixin, TemplateView):
+class HomePageView(TemplateView):
     template_name = "home.html"
-    redirect_field_name = "/accounts/login/"
