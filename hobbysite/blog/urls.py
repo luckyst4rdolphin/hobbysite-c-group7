4fdcd7346c40d60bf4c63b669774a754ea75f4a2
shamika-anne <shamanne.sawalha@gmail.com>
2025-05-12 15:10:36 +0800

    added url for update view

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
    path('articles/', ArticleListView.as_view(), name="article-list"),
    path('article/<int:pk>/', ArticleDetailView.as_view(), name="article-detail"),
    path('article/add/', ArticleCreateView.as_view(), name="article-form"),
+   path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name="article-update")
 ]
 
 app_name = "blog"
