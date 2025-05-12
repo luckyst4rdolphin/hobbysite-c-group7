0e099c46bfae1ce6c34b348958142e002d011d6c
shamika-anne <shamanne.sawalha@gmail.com>
2025-05-12 15:10:19 +0800

    added update view for articles

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import ModelFormMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from blog.models import Article, ArticleCategory, Comment
 from user_management.models import Profile
 from .forms import ArticleForm, CommentForm
 from django.urls import reverse_lazy
 
-class ArticleListView(LoginRequiredMixin, ListView):
+class ArticleListView(ListView):
     '''
     Creates a base view for displaying a list of objects.
     '''
@@ -21,7 +21,7 @@ class ArticleListView(LoginRequiredMixin, ListView):
         context['profile'] = Profile.objects.all()
         return context
 
-class ArticleDetailView(LoginRequiredMixin, DetailView, ModelFormMixin):
+class ArticleDetailView(DetailView, ModelFormMixin):
     '''
     Creates a base view for displaying the complete article.
     '''
@@ -71,5 +71,23 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(ArticleCreateView, self).get_context_data(**kwargs)
+        context['article_form'] = context['form']
+        return context
+    
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Article
+    template_name = "article_form.html"
+    form_class = ArticleForm
+
+    def get_success_url(self):
+        return reverse_lazy('blog:article-detail', kwargs={'pk': self.object.pk})
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ArticleUpdateView, self).get_context_data(**kwargs)
         context['article_form'] = context['form']
         return context
