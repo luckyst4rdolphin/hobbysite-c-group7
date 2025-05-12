dfd21e93f23260b3dca027b74d5b58a13957f11b
shamika-anne <shamanne.sawalha@gmail.com>
2025-05-12 14:29:58 +0800

    created model form for comments

--- hobbysite/blog/forms.py
+++ hobbysite/blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields =['title', 'category', 'header_image', 'entry']
+
+class CommentForm(forms.ModelForm):
+    entry = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Say something',}))
+    class Meta:
+        model = Comment
+        fields = ['entry']
