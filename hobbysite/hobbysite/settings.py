e539ed991ca2b92fca29a92f45b43703a668a9de
macasalad <hannahleibaniqued12@gmail.com>
2025-05-12 18:58:01 +0800

    Hid secret key

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-veh(zl9cfihr87c8_*^s!w=j99l2lcc(zq6_z_k*at0d$d&&^w'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
