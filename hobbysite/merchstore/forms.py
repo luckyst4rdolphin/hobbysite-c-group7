c8d4f3c476ff1a9322f985787d3906a46a5be171
macasalad <hannahleibaniqued12@gmail.com>
2025-05-12 14:08:18 +0800

    Added docstrings for forms.py

--- hobbysite/merchstore/forms.py
+++ hobbysite/merchstore/forms.py
@@ -2,11 +2,19 @@ from django import forms
 from .models import Product, Transaction
 
 class ProductForm(forms.ModelForm):
+    '''
+    Form for creating or updating a product.
+    Excludes the 'owner' field, which is set automatically in the view.
+    '''
     class Meta:
         model = Product
         exclude = ['owner']
 
 class TransactionForm(forms.ModelForm):
+    '''
+    Form for creating a transaction.
+    Restricts amount to a minimum value of 1.
+    '''
     amount = forms.IntegerField(min_value=1)
     class Meta:
         model = Transaction
