2cbc188d710265f4bc064a70b4706944995d57ec
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-05-12 10:10:43 +0800

    updated commission views

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,22 +1,59 @@
-from django.views.generic import ListView, DetailView
-from commissions.models import Commission, Comment
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from commissions.models import Commission, Job, JobApplication
+from commissions.forms import CommissionForm
+from django.db import models
 
 class CommissionListView(ListView):
     '''
-    Displays a list of all commissions, ordered by their creation date in ascending order.
+    Displays a list of all commissions, ordered by status (Open > Full > Completed > Discontinued),
+    then by creation date (most recent first).
     '''
     context_object_name = "commissions"
-    queryset = Commission.objects.all().order_by('created_on')
+    queryset = Commission.objects.all().order_by(
+        models.Case(
+            models.When(status='Open', then=0),
+            models.When(status='Full', then=1),
+            models.When(status='Completed', then=2),
+            models.When(status='Discontinued', then=3),
+            default=4,
+            output_field=models.IntegerField(),
+        ).desc(),  # most recent Open first
+        '-created_on'
+    )
     template_name = "commission_list.html"
 
 class CommissionDetailView(DetailView):
     '''
-    Displays the details of a specific commission, including its associated comments.
-
-    The comments are filtered to only include those belonging to the commission being viewed 
-    and are displayed in descending order based on their creation date (newest first).'
+    Displays the details of a specific commission, including its associated jobs.
     '''
     context_object_name = "commission"
     model = Commission
-    extra_context = {"comments":Comment.objects.all()}
+    extra_context = {"jobs":Job.objects.all()}
     template_name = "commission.html"
+
+class CommissionCreateView(LoginRequiredMixin, CreateView):
+    model = Commission
+    form_class = CommissionForm
+    template_name = "commission_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.use.profile
+        return super().form_valid(form)
+
+class CommissionUpdateView():
+    model = Commission
+    form_class = CommissionForm
+    template_name = "commission_form.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Auto-update commission status to Full if all jobs are full
+        jobs = Job.objects.filter(commission=self.object)
+        if jobs.exists() and all(job.status == 'Full' for job in jobs):
+            self.object.status = 'Full'
+            self.object.save()
+
+        return response
